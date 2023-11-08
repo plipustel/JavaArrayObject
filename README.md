4f3377f18e7a1077effc3a13c6f44220be1bf39d
Plipus Telaumbanua <plipus.tel@gmail.com>
2023-11-09 01:15:12 +0700

    Readme updated

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # JavaArrayObject
  
+<code>in React i can create array object like this:
+ 
+	const[cars, setCar] = useState(
+	[{
+		model: 'SUV',
+		year:'2023',
+		brand:'Toyota',
+		audio:[{
+				usb:true,
+				bluetooh: true,
+				radio:false,
+			  }, {
+				usb:true,
+				bluetooh: false,
+				radio:false,
+				}]
+	},
+	{
+		model: 'SUV',
+		year:'2012',
+		brand:'Rubicon',
+		audio:[{
+				usb:true,
+				bluetooh: true,
+				radio:true,
+			  }, {
+				usb:true,
+				bluetooh: false,
+				radio:true,
+			}]
+	 }
+	])
+ </code>
--- src/ArrayCars.java
+++ src/ArrayCars.java
@@ -4,39 +4,6 @@
  * @author: Plipus Telaumbanua
  * @created: 09 Nov 2023
  * 
- * 
- * in React i can create array object like this:
- * 
-	const[cars, setCar] = useState(
-	[{
-		model: 'SUV',
-		year:'2023',
-		brand:'Toyota',
-		audio:[{
-				usb:true,
-				bluetooh: true,
-				radio:false,
-			  }, {
-				usb:true,
-				bluetooh: false,
-				radio:false,
-				}]
-	},
-	{
-		model: 'SUV',
-		year:'2012',
-		brand:'Rubicon',
-		audio:[{
-				usb:true,
-				bluetooh: true,
-				radio:true,
-			  }, {
-				usb:true,
-				bluetooh: false,
-				radio:true,
-			}]
-	 }
-	])
  */
 
 
